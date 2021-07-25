a299d1035f9eb9ca96be5170b9dd5bb4880e3973
AHG <ahakangenc92@gmail.com>
2021-07-25 12:06:56 -0500

    Fix eslint no-unused-vars error

--- src/components/player/index.js
+++ src/components/player/index.js
@@ -1,3 +1,4 @@
+/* eslint no-unused-vars : "off" */
 import React, { useState, useContext, createContext } from 'react';
 import ReactDOM from 'react-dom';
 import { Container, Button, Overlay, Inner, Close } from './styles/player';
